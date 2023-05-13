0bcd4c16b931917cdab924497898004e98c35aa7
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-05-13 12:17:18 +0530

    Made dashboard

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$("#search-icon").click(function () {
+("#search-icon").click(function () {
     $(".nav").toggleClass("search");
     $(".nav").toggleClass("no-search");
     $(".search-input").toggleClass("search-active");
   });
 
-  $('.menu-toggle').click(function () {
+  ('.menu-toggle').click(function () {
     $(".nav").toggleClass("mobile-nav");
     $(this).toggleClass("is-active");
   });
